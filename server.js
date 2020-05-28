a6a6f1d7f440c5be64c7493e3ec0799f86314b3d
steviesocks <stephen.stocking@gmail.com>
2020-05-27 21:14:23 -0400

    updating database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-closed-68683',
     user : 'stephenstocking',
     password : '',
     database : 'smart-brain'
